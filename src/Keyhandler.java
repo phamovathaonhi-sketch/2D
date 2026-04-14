1677c47ce99b199a5a035350c4e52f2bdb1770a4
Thao Nhi Pham <phamovathaonhi@gmail.com>
2026-04-14 20:38:53 +0200

    .

---
+++ src/Keyhandler.java
@@ -0,0 +1,53 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Keyhandler implements KeyListener {
+    public boolean upressed;
+    public boolean downpressed;
+    public boolean rightpressed;
+    public boolean leftpressed;
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_DOWN){
+            downpressed= true;
+            
+        }
+        if (code == KeyEvent.VK_UP){
+            upressed= true;
+
+        }
+        if (code == KeyEvent.VK_RIGHT){
+            rightpressed= true;
+
+        }
+        if (code == KeyEvent.VK_LEFT){
+            leftpressed = true;
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_DOWN){
+            downpressed= false;
+
+        }
+        if (code == KeyEvent.VK_UP){
+            upressed= false;
+
+        }
+        if (code == KeyEvent.VK_RIGHT){
+            rightpressed= false;
+
+        }
+        if (code == KeyEvent.VK_LEFT){
+            leftpressed = false;
+        }
+    }
+}
--- src/S1.java
+++ src/S1.java
@@ -5,13 +5,23 @@ public class S1 extends JPanel implements Runnable {
     private JFrame jframe;
     private JScrollPane scrollPane;
     Thread gamethread;
-    private int orihi =16;
-    private int scale= 3;
-    private int tileSize = orihi*scale;
+    private int origi =10;
+    private int scale= 5;
+    private int tileSize = origi*scale;
+    //FPS
+    int FPS = 60;
+    Keyhandler K = new Keyhandler();
+
+    // player default position
+    int playerX= 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public S1(){
         jframe = new JFrame("Menu");
         init();
+        this.addKeyListener(K);
+        this.setFocusable(true);
     }
     public void init(){
         jframe.setSize(800, 600);
@@ -29,20 +39,43 @@ public class S1 extends JPanel implements Runnable {
 
     @Override
     public void run() {
+        double drawinterval = 1000000000/FPS;
+        double nextdrawtime = System.nanoTime() + drawinterval;
         while(gamethread !=null){
 
             update();
             repaint();
+            try {
+                double remaining = nextdrawtime - System.nanoTime();
+                remaining= remaining/1000000;
+                if (remaining < 0){
+                    remaining = 0;
+                }
+                gamethread.sleep((long) remaining);
+
+                nextdrawtime += drawinterval;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
     public void update(){
+        if (K.upressed == true){
+            playerY = playerY-playerSpeed;
+        } else if (K.downpressed == true) {
+            playerY = playerY+playerSpeed;
+        } else if (K.rightpressed == true) {
+            playerX = playerX + playerSpeed;
+        } else if (K.leftpressed == true) {
+            playerX = playerX - playerSpeed;
+        }
 
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         g2.setColor(Color.black);
-        g2.fillRect(100,100,tileSize,tileSize);
+        g2.fillRect(playerX,playerY,tileSize,tileSize);
         g2.dispose();
 
     }
